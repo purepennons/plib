dc9907ba7fa74e005df1dd8882792c5c32d89144
Chiahao Lin <y80811@gmail.com>
2015-11-11 12:11:25 +0800

    update testing

--- test/test.js
+++ test/test.js
@@ -29,6 +29,25 @@ describe('#mergeByKey', function() {
     let obj = mo.mergeByKey(obj1, obj2);
 
     expect(obj).to.be.an('object');
+    expect(obj).to.deep.equal({a: 40, b: 70});
 
   });
+
+  it('Input: deepObj1, deepObj2, no callback. it should return a object which values are aggregate by same key.', function() {
+    let obj = mo.mergeByKey(deepObj1, deepObj2);
+
+    expect(obj).to.be.an('object');
+    expect(obj).to.deep.equal({
+      a: 30,
+      b: {
+        c: 60,
+        d: {
+          e: 90
+        }
+      }
+    });
+
+  });
+
+
 });
